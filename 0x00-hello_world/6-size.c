0d13c83173229aeb0c1cba8151c4f5caea59fe04
root <root@ubuntu-xenial.com>
2020-06-10 22:17:02 +0000

    Low level programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdin.h>
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %i byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %i bytes(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", (int) sizeof(float));
+
+	return (0);
+}
