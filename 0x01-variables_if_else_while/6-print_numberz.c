018c4aaa6970096b46c4d38fcf2c2ec9e9452165
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-11 22:53:12 +0000

    C-programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Print digits base 10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		putchar(digit + '0');
+
+	putchar('\n');
+
+return (0);
+}
