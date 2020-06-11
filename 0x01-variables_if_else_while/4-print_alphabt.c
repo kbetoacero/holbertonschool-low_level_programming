460fafae18cc9cc09e2decd419d2652671a8be26
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-11 22:34:40 +0000

    C-programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+* main - Print the alphabet in lowercase except 'q' and 'e'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
