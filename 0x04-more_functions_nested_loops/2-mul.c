b13c8844f81770ac8093dae1c819484b64985179
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-18 18:07:16 +0000

    More Functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* mul - multiplies two integers 
+* @a: first integer
+* @b: second integer
+* Return: mul a * b
+*/
+int mul(int a, int b)
+{
+	int res = a * b;
+	return (res);
+}
