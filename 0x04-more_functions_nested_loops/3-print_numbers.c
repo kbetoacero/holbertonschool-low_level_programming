06fc0c2d6f6975fce9a52cd5e5e400a00c6fd6b2
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-18 18:53:14 +0000

    More Functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* print_numbers -  prints numbers 0 to 9
+* followed by a new line
+* @n: number to print
+* Return : Always 0
+*/
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{	
+	_putchar(n + '0');
+	n++;
+	}
+	_putchar('\n');
+}
