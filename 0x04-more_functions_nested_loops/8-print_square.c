e3cc331a525f16ef6b34726f1f9db3e85f4af9df
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-19 01:50:30 +0000

    More Functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+* print_square - print a square
+* @n: square
+* Return: Always 0
+*/
+
+void print_square(int size)
+{
+	int l = 1;
+
+	if ( size > 0)
+	{
+		while (l <= size)
+		{
+			int c = 1;
+
+			while (c <= size)
+			{
+				_putchar('#');
+				c++;
+			}
+			_putchar('\n');
+			l++;
+		}
+	}
+	else
+		_putchar('\n');
+}
