94018fffec85ca4058423fd8c9f70f387baf837b
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-20 18:25:08 +0000

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+* times_table - prints the 9 times table
+*
+*/
+
+void times_table(void)
+{
+	int columns;
+	int files = 0;
+	int d;
+
+	while (files < 10)
+	{
+		columns = 0;
+
+		while (columns < 10)
+		{
+			d = columns * files;
+
+			if (d < 10)
+			{
+				if (columns > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(d + '0');
+			}
+
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(d / 10 + '0');
+				_putchar(d % 10 + '0');
+			}
+		columns++;
+		}
+	_putchar('\n');
+	files++;
+	}
+	_putchar('\n');
+}
