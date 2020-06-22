9bdc4b353ad8251aa32d84e35c639f1c5f0a25ab
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-22 20:43:01 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+
+/**
+* reset_to_98 - takes a pointer to a int as a parameter n updates the value
+*it points to 98
+* @n: integer
+* Return : Always 0
+*/
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
