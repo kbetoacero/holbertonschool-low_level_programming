7553f7f2f0e14afff3a2dc761a505c69a550e1bb
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-24 01:37:46 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_array - prints n elements of an array of integers
+* @a : array
+* @n : number of elements
+* Return : void
+*/
+
+void print_array(int *a, int n)
+{
+
+	int nu  = 0;
+	
+	while (nu < n)
+	{
+		if (nu < n - 1)
+		{
+			printf("%d, ", a[nu]);
+		}
+		else
+		{
+			printf("%d", a[nu]);
+		}
+	nu++;
+	}
+	printf("\n");
+}
+
