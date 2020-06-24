c83fb3af039f53a72f45fbf966e9dba53f4529fe
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-24 21:06:19 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+* rev_string - reverses a string
+* @s - var type char
+* Return : reversed string
+*/
+
+void rev_string(char *s)
+{
+	char swap;
+	char *last = s;
+
+	last = s;
+
+	while (*last != '\0')
+	{
+	last++;
+	}
+	last--;
+
+	while (s < last)
+	{
+	swap = *s;
+	*s = *last;
+	*last = swap;
+	s++;
+	last--;
+	}
+}
+
