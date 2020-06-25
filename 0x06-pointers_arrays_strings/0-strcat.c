f7acc1841f7616b0ab1e97809257a7cb711cc904
vagrant <vagrant@vagrant-ubuntu-trusty-64.com>
2020-06-25 17:38:20 +0000

    Pointers, arrays and strings 2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int d = 0;
-	int s = 0;
+	int s1 = 0;
+	int s2 = 0;
 
-	while (dest[d])
+	while (dest[s1])
 	{
-		d++;
+		s1++;
 	}
-	while (src[s])
+	while (src[s2])
 	{
-		dest[d] = src[s];
-		d++;
-		s++;
+		dest[s1] = src[s2];
+		s1++;
+		s2++;
 	}
-	dest[d] = '\0';
+	dest[s1] = '\0';
 	return (dest);
 }
