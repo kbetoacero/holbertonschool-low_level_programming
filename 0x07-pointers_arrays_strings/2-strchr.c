566d0cb39d63eddc8598ac562367d2a76dbf6328
root <root@ubuntu-xenial.com>
2020-06-30 23:10:51 +0000

    Pointers, arrays and strings 3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* _strchr - locates a character in a string
+*@s : string
+*@c : character occurence
+* Return: A pointer to the first occurrence of the caracter c or NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+		{
+			return ('\0');
+		}
+		s++;
+	}
+}
