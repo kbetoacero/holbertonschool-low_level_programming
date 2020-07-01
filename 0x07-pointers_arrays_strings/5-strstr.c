27b6c321f1a809871a2c3f727b644041f94d3915
root <root@ubuntu-xenial.com>
2020-07-01 00:06:16 +0000

    Pointers, arrays and strings 3

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+* _strstr - find the ocurrence of the substring
+* @needle : pointer to the sunstring to find
+* @haystack : pointer to the string to search
+*
+* Return: a pointer to the beginning of the located substring,
+* or NULL if the substring is not found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	if (*needle == '\0')
+	{
+		return (0);
+	}
+	while (needle[i])
+	{
+		while (*haystack)
+		{
+			if (*haystack == needle[i])
+			{
+				return (haystack);
+			}
+			haystack++;
+		}
+		i++;
+	}
+	return ('\0');
+}
