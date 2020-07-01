c13aacde001aa91ca7cfee13496dfeed3e0d1b67
root <root@ubuntu-xenial.com>
2020-07-01 23:53:28 +0000

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _puts_recursion - prints a string followed by a new line
+* Return: 0
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
