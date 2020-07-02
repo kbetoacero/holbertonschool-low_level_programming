7a4a244ef61080958a037d08592e9c7cb8f72ecc
root <root@ubuntu-xenial.com>
2020-07-02 23:59:33 +0000

    Pointers, arrays and strings 3

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _pow_recursion - raise x to the power of y
+* @x : number to raise
+* @y : power
+*
+* Return: the values of x reaise to te power of y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
