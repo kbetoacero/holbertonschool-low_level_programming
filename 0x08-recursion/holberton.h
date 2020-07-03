7726ab6123a94d9138720d19aa71e8a6a1728ce1
root <root@ubuntu-xenial.com>
2020-07-03 00:04:20 +0000

    Pointers, arrays and strings 3

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -5,10 +5,7 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
-<<<<<<< HEAD
 int _pow_recursion(int x, int y);
-=======
 int factorial(int n);
->>>>>>> bcf8ea824076043dd1658e101415ff4183e43741
 
 #endif /*HOLBERTON_*/
