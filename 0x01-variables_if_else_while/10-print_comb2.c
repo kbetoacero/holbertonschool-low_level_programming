9a6d3abcc2b0f1c0a4414d1e4ad71c466f918abe
root <root@ubuntu-xenial.com>
2020-07-04 19:20:00 +0000

    C-programming

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,26 +7,30 @@
  */
 
 int main(void)
-
 {
-	int digit1;
-	int digit2;
 
-	for (digit2 = 0; digit2 <= 9; digit2++)
+	int digit2 = 0;
+	int digit1;
+
+	while (digit2 <= 9)
 	{
-		for (digit1 = 0; digit1 <= 9; digit1++)
+		digit1 = 0;
+
+		while(digit1 <= 9)
 		{
+
 			putchar(digit2 + '0');
 			putchar(digit1 + '0');
 
-			if (!(digit2 == 9 && digit1 == 9))
+			if (digit2 + digit1 != 18)
 			{
-				putchar(',');
-				putchar(' ');
+			putchar(',');
+			putchar(' ');
 			}
+			digit1++;
 		}
+		digit2++;
 	}
 	putchar('\n');
-
-return (0);
+        return (0);
 }
