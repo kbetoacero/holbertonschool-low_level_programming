f505aa141cd8ab5b4bffcff9cf815916003844c4
root <root@ubuntu-xenial.com>
2020-07-05 18:34:00 +0000

    Argc-Argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* 1-args.c - prints the number of arguments passed into it
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;	
+	printf("%d\n", argc - 1);
+	return (0);
+}
