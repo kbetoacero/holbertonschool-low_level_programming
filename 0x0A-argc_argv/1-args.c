42f284a84300eb7fe35eba0e698a76e83f0d538e
root <root@ubuntu-xenial.com>
2020-07-05 18:36:06 +0000

    Argc-Argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 
 /**
-* 1-args.c - prints the number of arguments passed into it
+* main - prints the number of arguments passed into it
+* @argc : Number of command line arguments
+* @argv : An array containing the program command line arguments
 * Return: 0
 */
 
 int main(int argc, char *argv[])
 {
-	(void)argv;	
+	(void)argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
