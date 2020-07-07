18912112b093c546235a02c0aab4bc3ff1808a10
root <root@ubuntu-xenial.com>
2020-07-07 23:42:15 +0000

    Malloc-free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+
 /**
 * *_strdup - return a pointer to a newlly allocated space in memory which
 * contains a copy of the string given as a parameter
@@ -26,6 +27,7 @@ char *_strdup(char *str)
 
 	if (s2 == NULL)
 		return (NULL);
+
 	i = 0;
 
 	while (i < len + 1)
