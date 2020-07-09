600fe4b9a6a63b3af989057b6a3e0842a7ac858a
root <root@ubuntu-xenial.com>
2020-07-09 23:25:00 +0000

    C-programming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible different combinations of two digits
+* Returns : 0
+*/
+
+int main()
+{
+
+	int d1 = 0;
+	int d2 = 1;
+
+	while (d1 < 10)
+	{
+		while (d2 < 10)
+		{
+			if (d1 < d2)
+			{
+				putchar(d1 + '0');
+				putchar(d2 + '0');
+				if (d1 != 8 || d2 != 9)
+				{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		d2++;
+	}
+	d1++;
+	d2 = 0;
+	}
+	return 0;
+}
