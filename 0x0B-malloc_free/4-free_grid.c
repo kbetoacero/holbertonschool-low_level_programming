0adc491f767003dc2719e67a5deb684797726883
root <root@ubuntu-xenial.com>
2020-07-10 02:35:02 +0000

    Malloc-free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* free_grid - frees 2 dimen grid
+* @grid: grid1
+* @height: column
+*/
+
+
+
+void free_grid(int **grid, int height)
+{
+	if (**grid && height)
+		return;
+	else
+		return;
+}
