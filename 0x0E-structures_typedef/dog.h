d176f8f5ac1d4ab36a7e65c9ea6b5cf570e4ad44
root <root@ubuntu-xenial.com>
2020-07-13 21:48:57 +0000

    Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: shows some informations about a dog
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
