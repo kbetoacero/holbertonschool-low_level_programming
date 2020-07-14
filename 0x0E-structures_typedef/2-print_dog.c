0c8420978d921cfb87c65a3acbde9b4e915b0086
root <root@ubuntu-xenial.com>
2020-07-14 01:35:29 +0000

    Structures, typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - prints a struct dog
+* @d: pointer
+*
+* Description: Prints a struct dog
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if ((d->name) == NULL)
+	{
+		d->name = "(nil)";
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+		printf("Age: %f\n", d->age);
+
+	if ((d->owner) == NULL)
+	{
+		d->owner = "(nil)";
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
