1589eadae69e54eff8bd63fa7a16f643260dce9b
root <root@ubuntu-xenial.com>
2020-07-16 03:00:35 +0000

    Function Pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,14 @@
 #include "function_pointers.h"
 
 /**
-* print_name - prints a name
-* @name: pointer of the name to print
-* @f: pointer to a function
-*
-* Return: void
-*/
-
-void print_name(char *name, void(*f)(char *))
+ * print_name - prints a name in different ways
+ * @name: pointer of the name to print
+ * @f: pointer to the function that prints the name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
 {
-
 	if (f && name)
 		f(name);
 }
