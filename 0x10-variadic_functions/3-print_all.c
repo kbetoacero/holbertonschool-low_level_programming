2e46d6c2d29049779fb056d77fa5346e015da719
root <root@ubuntu-xenial.com>
2020-07-19 18:55:49 +0000

    Variadic Functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,96 @@
+#include "variadic_functions.h"
+
+/**
+* print_all - print char, int, float or string
+* @format: string of formats
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	unsigned int i, j;
+	char *sep;
+
+	args_a li[] = {
+		{"c", li_c},
+		{"i", li_i},
+		{"f", li_f},
+		{"s", li_s},
+		{NULL, NULL},
+	};
+
+	sep = ", ";
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j <= 3)
+		{
+		if (*(li[j].st) == format[i])
+		{
+			li[j].l(ap);
+
+			if (format[i + 1])
+			{
+				printf("%s", sep);
+			}
+		}
+		j++;
+		}
+	i++;
+	}
+	printf("\n");
+	va_end(ap);
+
+}
+
+/**
+* li_c - print char type element
+* @list: va_list passed to function
+*/
+
+
+void li_c(va_list list)
+{
+	printf("%c", (char)va_arg(list, int));
+}
+
+/**
+* li_i - print int type element
+* @list: va_list passed to function
+*/
+
+
+void li_i(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+
+/**
+* li_f - print float type element
+* @list: va_list passed to function
+*/
+
+
+void li_f(va_list list)
+{
+	printf("%f", (float)va_arg(list, double));
+}
+
+/**
+* li_s - print string type element
+* @list: va_list passed to function
+*/
+
+void li_s(va_list list)
+{
+	char *s;
+
+	s = va_arg(list, char *);
+
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
