9d4c645a43cc69b0efc42554e818262242f43ad2
root <root@ubuntu-xenial.com>
2020-07-19 18:56:07 +0000

    Variadic Functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,10 +5,21 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+typedef struct args
+{
+	char *st;
+	void (*l)(va_list list);
+}args_a;
+
+ 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void li_c(va_list list);
+void li_i(va_list list);
+void li_f(va_list list);
+void li_s(va_list list);
 
 #endif /* VARIADIC_FUNCTIONS_H */
