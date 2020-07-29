079abb0f0bc22d0127ed17da9c09304b4f550523
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-07-29 14:37:39 +0000

    8-print_diagsums.c added

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of
+ * a square matrix of integers
+ * @a: matrix´ pointer
+ * @size: Size of one of the sides of the matrix
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, diag1 = 0, diag2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		diag1 = diag1 + a[i * (size + 1)];
+		diag2 = diag2 + a[i * (size + 1) + (size - 1) - i * 2];
+	}
+	printf("%d, %d\n", diag1, diag2);
+}
