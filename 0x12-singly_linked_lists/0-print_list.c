b909c84795465890b229a445661e7194804158f0
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-01 15:38:57 +0000

    0-print_list.c betty fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 
-/* print_list - prints all the elements of a list_t list
+/**
+* print_list - prints all the elements of a list_t list
+* @h: pointer to head
 * Return: the number of nodes
 */
 
@@ -10,32 +12,12 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str== NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 			temp++;
 			h = h->next;
 	}
-	return(temp);
+	return (temp);
 }
-
-
-/**
-* _strlen - returns lenght of a string.
-* @s: string
-* Return: lenght of the string
-
-
-int _strlen(char *s)
-{
-	int len;
-
-
-	while (*s != NULL)
-	{
-		s++;
-		len++;
-	}
-	return (len);
-}*/
