58f611b5746a1d8c12496381df67776a7655d555
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-01 15:55:18 +0000

    betty fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,10 +14,12 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
-			temp++;
-			h = h->next;
+
+		temp++;
+		h = h->next;
 	}
 	return (temp);
 }
