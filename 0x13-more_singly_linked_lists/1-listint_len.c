0e71be8c380634972f7c86813347c33a62cfb705
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-12 22:13:13 +0000

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- * listint_len -
- *
- * Return: 
- */
+* listint_len - returns the number of elements in a linked list.
+* @h: linked lists
+* Return: number of elements in the list
+*/
+
 size_t listint_len(const listint_t *h)
 {
+	listint_t *ptr, initial_node;
+	size_t count;
+	
+	if (!h)
+		return (0);
 
+	initial_node = *h;
+	count = 0;
+
+	for (ptr = &initial_node; ptr; ptr = ptr->next)
+		count++;
+
+	return (count);
 }
