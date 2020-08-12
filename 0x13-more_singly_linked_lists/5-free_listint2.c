032f9ae75b9d3bf5b821108160fa8b26a544b502
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-12 22:29:31 +0000

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
- *
- * Return: 
+ * free_listint2 - free the list and null condition
+ * @head: double pointer of head to list
+ * Return: return none
  */
+
 void free_listint2(listint_t **head)
 {
+	listint_t *i;
 
+	if (!head)
+	{
+		return;
+	}
+
+	while (*head)
+	{
+		i = *head;
+		*head = i->next;
+		free(i);
+	}
 }
