4a93dc198365889518432ffc1e9b99078a1dd52b
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-12 22:16:23 +0000

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,11 +1,23 @@
 #include "lists.h"
 
 /**
- * add_nodeint -
- *
- * Return: 
- */
+* add_nodeint - adds a new node at the beginning of a linked list.
+* @head: head of the linked list
+* @n: int to add to the new node.
+* Return: the address of the new element, or NULL if it failed.
+*/
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *ptr;
 
+	ptr = malloc(sizeof(listint_t));
+	if (!ptr)
+ 		return (NULL);
+
+	ptr->n = n;
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
 }
