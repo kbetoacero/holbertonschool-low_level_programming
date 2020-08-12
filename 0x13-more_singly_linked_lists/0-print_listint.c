3b1631e168d737b9e126284bb43db75fb4d58d1a
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-12 22:06:00 +0000

    lists.h 0-print_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,26 @@
 #include "lists.h"
 
 /**
- * print_listint -
- *
- * Return: 
+* print_listint - prints all the elements of a listint_t list.
+* @h: linked list.
+* Return: number of nodes.
  */
+
 size_t print_listint(const listint_t *h)
 {
+	listint_t *ptr, initial_node;
+	size_t count;
 
+	if (!h)
+		return (0);
+
+	initial_node = *h;
+	count = 0;
+	for (ptr = &initial_node; ptr; ptr = ptr->next)
+	{
+		printf("%d\n", ptr->n);
+		count++;
+	}
+
+	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+  #include <stdio.h>
 #include <stdlib.h>
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
