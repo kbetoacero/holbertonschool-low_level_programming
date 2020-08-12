19b4b6fb6776267df94e683f42104d348857c7f7
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-12 22:33:17 +0000

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * pop_listint -
- *
- * Return: 
+ * pop_listint - deletes the head node of a listint_t linked list,
+ * and returns the head node’s data (n).
+ * @head: linked list head.
+ * Return: head node's data (n) or 0.
  */
 int pop_listint(listint_t **head)
 {
+	int data;
+	listint_t *ptr;
 
+	if (!(*head))
+		return (0);
+
+	data = (*head)->n;
+	ptr = (*head)->next;
+
+	free((*head));
+
+	*head = ptr;
+	
+	return (data);
 }
