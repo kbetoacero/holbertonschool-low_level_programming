51d974c6f457f29677647f112607ee5e37165722
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-12 22:26:40 +0000

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,28 @@
 #include "lists.h"
 
 /**
- * free_listint -
+ * free_listint - frees a linked list.
+ * @head: head ptr of the linked list.
  *
- * Return: 
+ * Return: Void
  */
 void free_listint(listint_t *head)
 {
+	listint_t *ptr;
+	int count, i;
 
+	if (!head)
+		return;
+	count = 0;
+	for (ptr = head; ptr; ptr = ptr->next)
+		count++;
+
+	for (; count; count--)
+	{
+		ptr = head;
+		for (i = 0; i < count; i++)
+			ptr = ptr->next;
+		free(ptr);
+	}
+	free(head);
 }
