51df17f5e0120b209d4e1f7921abd6a2b3688b30
root <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-12 22:10:16 +0000

    lists.h betty fixed

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*/
+
 typedef struct listint_s
 {
 	int n;
