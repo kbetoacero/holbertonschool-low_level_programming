1603bb536984d06b0efaf9f3ed9a0b8068288a64
vagrant <vagrant@vagrant-ubuntu-trusty64.com>
2020-08-15 15:23:45 +0000

    advanced

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,25 +4,29 @@
 /**
  * main - computes and prints the sum of all the
  * multiples of 3 or 5 below 1024 (excluded)
- * i: integer
- * r: Result
  * Return: On success 1.
  */
-
 int main(void)
 {
-	int i;
-	long a = 1, b = 2, r = 0;
+	long int i, j, k, next;
 
-	printf("1, 2");
-/*	_putchar(1, 2); */
-	for (i = 1; i <= 48; i++)
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
 	{
-		r = a + b;
-		a = b;
-		b = r;
-		printf(", %ld", r);
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
-	_putchar('\n');
+
 	return (0);
 }
