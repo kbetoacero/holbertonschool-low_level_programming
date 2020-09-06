8bc6582b13e7411ad557fbe8dd2ff1292690d3e5
vagrant <vagrant@vagrant-ubuntu-trusty64.com>
2020-09-06 23:20:45 +0000

    3-add_dnodeint_end.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - ads a node at the end of a dlistint_t list
+ * @head: head of the dlistint
+ * @n: number of nodes
+ * Return: the address of the new element, or NULL if it failed
+ **/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL;
+	dlistint_t *list = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new != NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	
+	if (head != NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		list = *head;
+		while (list->next != NULL)
+			list = list->next;
+		new->prev = list;
+		list->next = new;
+	}
+	return (new);
+}
