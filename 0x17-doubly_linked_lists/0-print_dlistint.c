91939a34255e805f75c423c5f9aaf21e4b3ccd80
vagrant <vagrant@vagrant-ubuntu-trusty64.com>
2020-09-06 21:48:42 +0000

    0-print_dlistint.c betty fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	int i = 0;
 	const dlistint_t *list = h;
 
-	while(list != NULL)
+	while (list != NULL)
 	{
 		printf("%i\n", list->n);
 		list = list->next;
