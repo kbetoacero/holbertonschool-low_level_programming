bb5903960e8d02e7c3120cfe3dc818dcd2133652
vagrant <vagrant@vagrant-ubuntu-trusty64.com>
2020-09-06 22:33:34 +0000

    2-add_dnodeint.c errir fixed

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,10 +12,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 
+	
 	if (new != NULL)
+	{
 		new->n = n;
 		new->next = *head;
 		new->prev = NULL;
+	}
+	else
+		return (NULL);
 
 	if (*head != NULL)
 		(*head)->prev = new;
