94377625f44af267b075eb9deadfb9068eb62de2
kbetoacero <vagrant@vagrant-ubuntu-trusty64.com>
2020-10-02 17:23:57 +0000

    3-hash_table_set.c memory leaks fixed and betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || ht == NULL || key == '\0')
 		return (0);
-	
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
