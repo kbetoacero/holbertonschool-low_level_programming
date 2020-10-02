e99a354060f493a9c97d4070f15d3553bf757a06
kbetoacero <vagrant@vagrant-ubuntu-trusty64.com>
2020-10-02 16:59:10 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -
- *
- * Return: 
+ * hash_table_create - creates a hash table
+ * @size: array's length
+ * Return: new table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new_table;
+	unsigned int i;
 
+	if (size < 1)
+		return (NULL);
 
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->array = malloc(sizeof(void *) * size);
+	if (new_table == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+
+	new_table->size = size;
+
+	return (new_table);
 }
