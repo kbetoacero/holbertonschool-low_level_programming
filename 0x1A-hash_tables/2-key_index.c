e814a32cdacf2a8fd1a2d03f6f7683480e772bc7
kbetoacero <vagrant@vagrant-ubuntu-trusty64.com>
2020-10-02 17:06:04 +0000

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,16 @@
 #include "hash_tables.h"
 
 /**
- * key_index -
- *
- * Return: 
+ * key_index - find index of a key
+ * @size: array`s size
+ * @key: Name of the key
+ * Return: key`s index
  */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	unsigned long int index;
 
-
+	index = hash_djb2(key);
+	return (index % size);
 }
