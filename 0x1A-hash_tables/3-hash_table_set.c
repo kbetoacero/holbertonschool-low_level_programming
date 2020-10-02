d73179bcd190582142029b02ed89cc5e0183521c
kbetoacero <vagrant@vagrant-ubuntu-trusty64.com>
2020-10-02 17:22:16 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,47 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set -
- *
- * Return: 
+ * hash_table_set - adds an element to the hash table
+ * @ht: added hash table
+ * @value: key value
+ * @key: new node's key
+ * Return: 1 on success 0 otherwise
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *new_node;
+	hash_node_t *tmp;
+	unsigned long int index;
+	char *node_key, *node_value;
 
+	if (key == NULL || ht == NULL || key == '\0')
+		return (0);
+	
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
 
+	index = key_index((const unsigned char *)key, ht->size);
+	node_key = strdup(key);
+	node_value = strdup(value);
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = node_value;
+			free(node_key);
+			free(new_node);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	new_node->key = node_key;
+	new_node->value = node_value;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
