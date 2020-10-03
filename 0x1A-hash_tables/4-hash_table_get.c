6fcaf8ffb0164471a34c5ee543bd1ec068803e89
kbetoacero <vagrant@vagrant-ubuntu-trusty64.com>
2020-10-03 00:00:20 +0000

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -
- *
- * Return: 
+ * hash_table_get - retrieves a value associeted with a key
+ * @ht: Searched hash table
+ * @key: searched key
+ * Return: key value
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *temp;
+	unsigned long int index;
 
+	if (ht == NULL || key == NULL || key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (ht->array[index] != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
 }
