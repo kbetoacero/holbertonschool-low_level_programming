8df4449526280ce1a3898a4beba2941e038854f6
kbetoacero <vagrant@vagrant-ubuntu-trusty64.com>
2020-10-03 00:02:10 +0000

    5-hash_table_print.c code fixed

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,34 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
- *
- * Return: 
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	int comma = 0;
+	unsigned int i;
+	hash_node_t *temp;
 
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			temp = ht->array[i];
+			while (temp != NULL)
+			{
+				if (comma == 1)
+					printf(", ");
+				printf("'%s': '%s'", temp->key, temp->value);
+				comma = 1;
+				temp = temp->next;
+			}
+		}
+	}
+	printf("}\n");
 }
