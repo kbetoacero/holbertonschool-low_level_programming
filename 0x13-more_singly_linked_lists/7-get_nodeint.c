29f30ffad16f22903227e84b124069a4e126cb93
kbetoacero <2028@holbertonschool.com>
2021-01-18 18:31:08 +0000

    7-get_nodeint.c corrected

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * get_nodeint_at_index - Return node at index
@@ -8,7 +11,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-		while (head != NULL && indexx--)
+		while (head != NULL && index--)
 		{
 				head = (head->next != NULL ? head->next : NULL);
 		}
