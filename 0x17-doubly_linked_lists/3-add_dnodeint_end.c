a06697413e8a47ae0f5eb8e02fe064da972606a6
kbetoacero <carlos.acero@holbertonschool.com>
2021-04-30 14:29:17 +0000

    doble linked list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,38 +1,11 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - ads a node at the end of a dlistint_t list
- * @head: head of the dlistint
- * @n: number of nodes
- * Return: the address of the new element, or NULL if it failed
- **/
-
+ * add_dnodeint_end -
+ *
+ * Return: 
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-	dlistint_t *list;
 
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-
-	if (*head != NULL)
-	{
-		list = *head;
-		while (list->next != NULL)
-		{
-			list = list->next;
-		}
-		list->next = new;
-		new->prev = list;
-	}
-	else
-	{
-		new->prev = NULL;
-		*head = new;
-	}
-	return (new);
 }
--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,18 +1,11 @@
 #include "lists.h"
 
 /**
- * free_dlistint - free a dlistint_t list
- * @head: head of a dllist_t
- * Return: Void
+ * free_dlistint -
+ *
+ * Return: 
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *list = head, *aux;
 
-	while (list != NULL)
-	{
-		aux = list->next;
-		free(list);
-		list = aux;
-	}
 }
